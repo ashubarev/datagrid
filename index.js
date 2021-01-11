154d7080f2c953f48f9d50d0d8288127ddffaf67
ashubarev <77190078+ashubarev@users.noreply.github.com>
2021-01-11 09:52:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+$(function() {
+    $("#dataGrid").dxDataGrid({
+        "dataSource": [
+            {
+                "ID": 1,
+                "CompanyName": "Super Mart of the West",
+                "City": "Bentonville",
+                "State": "Arkansas"
+            },
+            {
+                "ID": 2,
+                "CompanyName": "Electronics Depot",
+                "City": "Atlanta",
+                "State": "Georgia"
+            },
+            {
+                "ID": 3,
+                "CompanyName": "K&S Music",
+                "City": "Minneapolis",
+                "State": "Minnesota"
+            },
+            {
+                "ID": 4,
+                "CompanyName": "Tom's Club",
+                "City": "Issaquah",
+                "State": "Washington"
+            },
+            {
+                "ID": 5,
+                "CompanyName": "E-Mart",
+                "City": "Hoffman Estates",
+                "State": "Illinois"
+            }
+        ]
+    });
+});
